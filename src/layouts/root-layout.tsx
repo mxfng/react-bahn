4e2ed6bcb3cbaab3be46ad98ef582ff071d3347d
mxfng <maxhfung@gmail.com>
2025-06-02 21:55:51 -0700

    Update root layout

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -3,5 +3,17 @@ interface RootLayoutProps {
 }
 
 export const RootLayout = ({ children }: RootLayoutProps) => (
-  <main>{children}</main>
+  <div>
+    <header>
+      <div>
+        <nav>{/* Add your navigation items here */}</nav>
+      </div>
+    </header>
+
+    <main>{children}</main>
+
+    <footer>
+      <div>{/* Built with React Bahn */}</div>
+    </footer>
+  </div>
 );
